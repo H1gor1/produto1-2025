4db31285e8cbe50353cc6ae87a2fe6cfdd3d635d
Higor Gabriel <higorgabrieldev@gmail.com>
2025-04-01 15:15:57 -0300

    aula 03

--- src/main/java/edu/ifmg/produtos/services/CategoryService.java
+++ src/main/java/edu/ifmg/produtos/services/CategoryService.java
@@ -1,12 +1,16 @@
 package edu.ifmg.produtos.services;
 
+import edu.ifmg.produtos.dtos.CategoryDTO;
 import edu.ifmg.produtos.entities.Category;
 import edu.ifmg.produtos.repository.CategoryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Collections;
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 public class CategoryService {
@@ -15,7 +19,15 @@ public class CategoryService {
     private CategoryRepository categoryRepository;
 
     @Transactional(readOnly = true)
-    public List<Category> findAll(){
-        return categoryRepository.findAll();
+    public List<CategoryDTO> findAll(){
+        List<Category> list = categoryRepository.findAll();
+        return list.stream().map(x-> new CategoryDTO(x)).collect(Collectors.toList());
+    }
+
+    @Transactional(readOnly = true)
+    public CategoryDTO findById(Long id){
+        Optional<Category> obj = categoryRepository.findById(id);
+        Category category = obj.get();
+        return new CategoryDTO(category);
     }
 }
