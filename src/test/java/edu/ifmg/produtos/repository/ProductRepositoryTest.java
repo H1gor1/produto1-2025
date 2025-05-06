198cce637761be9f0d76fff31dfba65497f991a5
higor <higorgabrieldev@gmail.com>
2025-05-06 15:28:33 -0300

    aula 10 - camada de serviço

--- src/test/java/edu/ifmg/produtos/repository/ProductRepositoryTest.java
+++ src/test/java/edu/ifmg/produtos/repository/ProductRepositoryTest.java
@@ -1,6 +1,7 @@
 package edu.ifmg.produtos.repository;
 
 import edu.ifmg.produtos.entities.Product;
+import edu.ifmg.produtos.util.Factory;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -17,15 +18,34 @@ class ProductRepositoryTest {
     @Autowired
     private ProductRepository productRepository;
 
+    private Long existingId;
+
+    @BeforeEach
+    public void setUp()  throws Exception {
+        existingId = 1L;
+    }
+
     @Test
-    @DisplayName(value = "Verificando se o objeto não existe no" +
+    @DisplayName(value = "Verificando se o objeto não existe no " +
             "BD depois de deletado.")
     public void deleteShoudDeleteObjectWhenIdExists() {
 
-        productRepository.deleteById(1L);
-        Optional<Product> obj = productRepository.findById(1L);
+        productRepository.deleteById(existingId);
+        Optional<Product> obj = productRepository.findById(existingId);
 
         Assertions.assertFalse(obj.isPresent());
 
+    }
+    @Test
+    @DisplayName(value = "Verificando o auto incremento da chave primaria")
+    public void insertShoudPersistWithAutoIncrementIdWhenIdNull() {
+        Product product = Factory.createProduct();
+        product.setId(null);
+
+        Product p = productRepository.save(product);
+        Optional<Product> obj = productRepository.findById(p.getId());
+        Assertions.assertTrue(obj.isPresent());
+        Assertions.assertNotNull(obj.get().getId());
+        Assertions.assertEquals(26, obj.get().getId());
     }
 }
